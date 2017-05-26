232e0a1cd0bff20038a5a61bce401b0d1f0f4e1b
huayl <cnkernel@gmail.com>
2017-05-27 04:52:30 +0800

    modify update

--- sbin/update.sh
+++ sbin/update.sh
@@ -2,18 +2,19 @@
 
 source $GOSBIN/env.sh
 
-function _install() {
-	go get -u $1
-	go install -v $1
+function _update()
+{
+	pkgfile=$GOPATH/config/pkg.ini
+	while read line
+	do
+		remark=`echo ${line}	| grep "^\s*#"`
+		if [ "${remark}" != "" ];then
+			continue
+		fi
+		go get -u $line
+		go install -v $line
+	done < ${pkgfile}
 }
 
-#_install google.golang.org/grpc
-_install github.com/golang/protobuf/proto
-_install github.com/golang/protobuf/protoc-gen-go
-#_install github.com/spf13/cobra/cobra
-#_install github.com/hashicorp/consul
-#_install github.com/hashicorp/consul-template
-_install github.com/nsf/gocode
-_install github.com/rogpeppe/godef
-#_install golang.org/x/tools/cmd/goimports
+_update() $@
 
