dc302d4cd185822eaab1b2ed05955c126a733baf
root <root@localhost.localdomain>
2017-06-21 10:56:18 +0800

    linux add

--- sbin/update.sh
+++ sbin/update.sh
@@ -7,13 +7,13 @@ function _install() {
 	go install -v $1
 }
 
-_install google.golang.org/grpc
+#_install google.golang.org/grpc
 _install github.com/golang/protobuf/proto
 _install github.com/golang/protobuf/protoc-gen-go
-_install github.com/spf13/cobra/cobra
-_install github.com/hashicorp/consul
-_install github.com/hashicorp/consul-template
+#_install github.com/spf13/cobra/cobra
+#_install github.com/hashicorp/consul
+#_install github.com/hashicorp/consul-template
 _install github.com/nsf/gocode
 _install github.com/rogpeppe/godef
-_install golang.org/x/tools/cmd/goimports
+#_install golang.org/x/tools/cmd/goimports
 
